604285ced934319707666c13d2cbb69603f61728
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2022-10-10 16:26:37 +0600

    Time: 875 ms (5.42%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 
-select email from(
-select email, count(email) as num from Person
-group by email) as statistic
-where num > 1;
+select email from Person
+group by email
+having count(Email) > 1;
